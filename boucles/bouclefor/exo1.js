4750f6afa4e1948236eb64586c7fb6c569399180
mugi93 <g.mugi93140@gmail.com>
2021-03-12 17:30:19 +0100

    loop

--- boucles/bouclefor/exo1.js
+++
@@ -1,7 +0,0 @@
-var result = 0;
-for ( var i = 5; i <= 10;i++){
-
-    var carre = i*i;
-    result = carre + result;
-}
-console.log(result);
---
+++ boucles/bouclefor/forLoop.js
@@ -0,0 +1,21 @@
+
+// exo 1
+
+var result = 0;
+for ( var i = 5; i <= 10;i++){
+
+    var carre = i*i;
+    result = carre + result;
+}
+console.log(result);
+
+
+
+// exo 2
+
+var max = 1000;
+ for (var i =100; i<=max;i++){
+     if(i % 7 ===0){
+         console.log(i)
+     }
+ }

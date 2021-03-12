7e7c2f274cbe3f34c0e81b91ec958db862d37602
mugi93 <g.mugi93140@gmail.com>
2021-03-12 14:03:10 +0100

    bonus

--- jour-4/bonus.js
+++ jour-4/bonus.js
@@ -1,7 +1,25 @@
-function generatePassword(num){
-    while(num <=
+function generatePassword(num) {
+    var password2 = ""
+    
+    if(num<6 || num>15){
+        console.log("error")
+    
+    }else{for (var i =1; i <= num; i++) {
 
+        var pass = Math.floor(Math.random() * (90 - 65 + 1) + 65);
+        var password = String.fromCharCode(pass);
+        var password2 = password2 + password;
+        
+
+    }
+        console.log(password2);
+    }
+
+ 
 }
 
+generatePassword(6);
 
-//.fromcharcode
+// var pass = Math.floor(Math.random() * (90 - 65 + 1) + 65);
+// var password = String.fromCharCode(pass)
+// console.log(password);

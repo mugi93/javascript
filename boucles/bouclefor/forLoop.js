666dd1b9e9546f4a7abb4a4c4446300f3c7fa458
mugi93 <g.mugi93140@gmail.com>
2021-03-15 17:26:24 +0100

    e

--- boucles/bouclefor/forLoop.js
+++ boucles/bouclefor/forLoop.js
@@ -1,21 +1,44 @@
 
 // exo 1
 
-var result = 0;
-for ( var i = 5; i <= 10;i++){
+// var result = 0;
+// for ( var i = 5; i <= 10;i++){
 
-    var carre = i*i;
-    result = carre + result;
-}
-console.log(result);
+//     var carre = i*i;
+//     result = carre + result;
+// }
+// console.log(result);
 
 
 
-// exo 2
+// // exo 2
 
-var max = 1000;
- for (var i =100; i<=max;i++){
-     if(i % 7 ===0){
-         console.log(i)
+// var max = 1000;
+// var maxi = []
+//  for (var i =100; i<=max;i++){
+//      if(i % 7 ===0){
+//          console.log(i);
+//          maxi.push(i);
+
+//         }
+        
+
+//  }
+//  console.log(maxi.length);
+
+
+
+ // exo 3
+var sum=0;
+ for ( var i = 1;i<=20;i++){
+     var dice= Math.floor(Math.random()*(6-1+1)+1);
+     
+     if(dice>=5){
+        sum=sum+dice;
+        console.log(sum);
+
      }
- }
+      }
+      
+     
+

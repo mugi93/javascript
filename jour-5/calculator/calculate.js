e756da13475a0f914a7049ba677f5b9c743dd9c8
mugi93 <g.mugi93140@gmail.com>
2021-03-17 00:23:05 +0100

    pi

--- jour-5/calculator/calculate.js
+++ jour-5/calculator/calculate.js
@@ -2,19 +2,70 @@
 function calculate (test) {
 
   
-    // if (i > 0) {
-    //     console.log(parseInt(process.argv[2]) * parseInt(process.argv[3]));
-
-    // }
+    
 
     var args = process.argv.slice(2);
-    if (parseInt(process.argv[3])=== +){
+
+
+    if (process.argv[3]=== "+"){
+        console.log(parseInt(process.argv[2]) + parseInt(process.argv[4]));
+
+    }else if (process.argv[3]=== "-"){
+        console.log(parseInt(process.argv[2]) - parseInt(process.argv[4]));
+    }
+    else if (process.argv[3]=== "/"){
+        console.log(parseInt(process.argv[2]) / parseInt(process.argv[4]));
+    
+    }
+    else if (process.argv[3]=== "x"){
+        console.log(parseInt(process.argv[2]) * parseInt(process.argv[4]));
 
     }
-
     
-    console.log(parseInt(process.argv[2]) / parseInt(process.argv[3]));
 
 }
 
 calculate();
+
+
+
+// function calculate (num , num2, operateur){
+//     if(operateur= "+"){
+//         console.log(num + num2);
+    
+//     }else if(operateur="-"){
+//         console.log(num - num2);
+    
+//     }else if(operateur="/"){
+//         console.log(num / num2);
+    
+//     }else if(operateur="x"){
+//         console.log(num * num2);
+//     }
+
+    
+
+
+
+// }
+
+
+
+
+
+// function calculate (test) {
+
+  
+//     // if (i > 0) {
+//     //     console.log(parseInt(process.argv[2]) * parseInt(process.argv[3]));
+
+//     // }
+
+//     var args = process.argv.slice(2);
+
+    
+//     console.log(parseInt(process.argv[2]) / parseInt(process.argv[3]));
+
+// }
+
+// calculate();

134cdb4b588045342011a7868eda12408d2e6ebb
Mugi <g.mugi93140@gmail.com>
2021-03-18 17:20:48 +0100

    suppression node_modules

--- jour-6/validator.js
+++ jour-6/validator.js
@@ -11,3 +11,15 @@
 var prompt = require("prompt");
 
 prompt.start();
+
+
+
+prompt.get(["username", "email"], function (err, res) {  // demande à obtenir les propriétés "username" et "email"
+  if (err) {
+		return onErr(err);
+	}
+
+  console.log("Données reçues :");
+  console.log("=> Username : " + res.username); // affiche le résultat pour la propriété "username"
+  console.log("=> Email : " + res.email); // affiche le résultat pour la propriété "email"
+});

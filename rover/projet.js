2f9c31d105e6ce16b1c6fbbd928124fa912c431d
Mugi <g.mugi93140@gmail.com>
2021-03-26 17:28:28 +0100

    rover

--- rover/projet.js
+++ rover/projet.js
@@ -15,21 +15,28 @@ var rover = {
     direction: "N",
     x: 0,
     y: 0,
+    travelLog: [],
 };
+
+
+
+
+
+
 
 //fonction pour tourner à droite
 
 function turnRight(rover) {
     if (rover.direction === "N") {
         rover.direction = "E";
-    }else if (rover.direction==="E"){
-        rover.direction="S"
-    }else if(rover.direction==="S"){
-        rover.direction="W"
-    }else{
-        rover.direction="N"
+    } else if (rover.direction === "E") {
+        rover.direction = "S";
+    } else if (rover.direction === "S") {
+        rover.direction = "W";
+    } else {
+        rover.direction = "N";
     }
-    console.log(rover);
+    // console.log(rover);
 };
 // turnRight(rover);
 
@@ -39,51 +46,155 @@ function turnRight(rover) {
 function turnLeft(rover) {
     if (rover.direction === "N") {
         rover.direction = "W";
-    }else if (rover.direction==="W"){
-        rover.direction="S"
-    }else if(rover.direction==="S"){
-        rover.direction="E"
-    }else{
-        rover.direction="N"
+    } else if (rover.direction === "W") {
+        rover.direction = "S";
+    } else if (rover.direction === "S") {
+        rover.direction = "E";
+    } else {
+        rover.direction = "N";
     }
 
-    console.log(rover);
+    // console.log(rover);
 };
 // turnLeft(rover);
 
 // fonction pour bouger 
 function moveFoward(rover) {
-   
     if(rover.direction==="E"){
+        if(rover.x<9){
+            rover.x++
+            rover.travelLog.push("x:" + rover.x+","+ "y:" + rover.y);
+        }else {
+            console.log("error")
+            return
+        };
+    };
+    if(rover.direction==="W"){
+        if(rover.x>0){
+            rover.x++
+            rover.travelLog.push("x:" + rover.x+","+ "y:" + rover.y);
+        }else {
+            console.log("error")
+            return
+        };
+    };
+    if(rover.direction==="S"){
+        if(rover.y<9){
             rover.y++
-    }else if (rover.direction==="W"){
-            rover.y--
-    }else if (rover.direction==="S"){
-        rover.x++
-    }else{
-        rover.x--
-    }
+            rover.travelLog.push("x:" + rover.x+","+ "y:" + rover.y);
+        }else {
+            console.log("error")
+            return
+        };
+    };
+    if(rover.direction==="N"){
+        if(rover.x>0){
+            rover.y++
+            rover.travelLog.push("x:" + rover.x+","+ "y:" + rover.y);
+        }else {
+            console.log("error")
+            return
+        };
+    };
 
-    console.log(rover)
+
+
+
+    
+
+    //  if (rover.direction === "E" && rover.x<9) {
+    //     rover.x++;
+    // } else if (rover.direction === "W" && rover.x>0) {
+    //     rover.x--;
+    // } else if (rover.direction === "S" && rover.y<9) {
+    //     rover.y++;
+    // } else if(rover.direction==="N"&& rover.y>0){
+    //     rover.y--;
+    
+    // }else{
+        
+    //     return console.log("error")
+
+    // }
+
+
+    // console.log(rover);
 };
 
 // moveFoward(rover);
 
 
+// fonction pour reculer
+function moveBackWard(rover){
+    if (rover.direction==="N"&&rover.y<9){
+        rover.y++
+        rover.travelLog.push("x:" + rover.x+","+ "y:" + rover.y);
+    }else if(rover.direction==="S"&&rover.y>0){
+        rover.y--
+        rover.travelLog.push("x:" + rover.x+","+ "y:" + rover.y);
+    }else if(rover.direction==="E"&&rover.x>0){
+        rover.x--
+        rover.travelLog.push("x:" + rover.x+","+ "y:" + rover.y);
+    }else if(rover.direction==="W"&&rover.x<9){
+        rover.x++
+        rover.travelLog.push("x:" + rover.x+","+ "y:" + rover.y);
+    }
+
+}
+
+
 // fonction outil
 
-function pilotRover(pilot){
-    for(var i=0;i<pilot.length;i++){
-        if(pilot[i]==="l"){
-            turnLeft(rover)
-        }else if (pilot[i]==="r"){
-            turnRight(rover)
-        }else if (pilot[i]==="f"){
-            moveFoward(rover)
-        }else{
-            console.log("error")
-        }
+function pilotRover(pilot) {
+
+    for (var i = 0; i < pilot.length; i++) {
+
+        if (pilot[i] === "l") {
+            turnLeft(rover);
+        } else if (pilot[i] === "r") {
+            turnRight(rover);
+        } else if (pilot[i] === "f") {
+            moveFoward(rover);
+        } else if (pilot[i]=== "b"){
+            moveBackWard(rover)
+        }else {
+            console.log("error");
+            return
+        };
         
-    }
+          console.log(rover)
+    };
+    
+   
+   
+        
+
+
+};
+
+// pilotRover("ff");
+
+
+
+var prompt = require("prompt");
+var option = {
+    name: "direction",
+    validator: /^[a-zA-Z]/,
+    description: "utiliser juste les lettre r, l et f",
 }
-pilotRover("lpoo")
+
+prompt.get(option,
+    function (err, res) {
+        if (err) {
+            console.error(err);
+            return
+
+        }else {
+            pilotRover(res.direction)
+
+        };
+
+
+
+    });
+
